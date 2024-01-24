6593093f5a5e33b4483668c2f4dc2f58c54a465a
Virgílio César <enjels@outlook.com>
2024-01-24 17:23:18 -0300

    ajuste de codigo calc

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
+/* eslint-disable no-unused-vars */
+import {data} from "./data/data.js";
+import { useState } from 'react'
 import ImcCalc from './components/ImcCalc'
-
 import './App.css'
+
+
 
 function App() {
 
+  const calcImc = (e, height, weight) => {
+    e.preventDefault()
+
+    console.log(height, weight)
+    if (!weight || !height) return;
+
+    console.log("executou")
+
+  }
+
+  const [imc, setImc] = useState("")
+  const [info, setInfo] = useState("")
+  const [infoClass, setInfoClass] = useState("")
+  
   return (
-    <div className='container'>
-      <ImcCalc/>
+    <div className="container">
+      {!imc ? (
+        <ImcCalc calcImc={calcImc} />
+      ) : (
+          <ImcTable/>
+      )}
     </div>
-  )
+  );
 }
 
 export default App
--- src/components/ImcCalc.jsx
+++ src/components/ImcCalc.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Button from "./Button";
 import "./ImcCalc.css";
 
-const ImcCalc = () => {
+const ImcCalc = (calcImc) => {
   const [height,setHeight] = useState("")
   const [weight, setWeight] = useState("")
   
@@ -55,7 +55,7 @@ const ImcCalc = () => {
           </div>
         </div>
         <div className="action-control">
-          <Button id="calc-btn" text="Calcular" />
+          <Button id="calc-btn" text="Calcular" action={calcImc} />
           <Button id="clear-btn" text="Limpar" action={clearForm} />
         </div>
       </form>
