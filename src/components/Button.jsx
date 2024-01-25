a9245c6a868c0667e9bf92dea15cda53a79d0458
Virgílio César <enjels@outlook.com>
2024-01-25 16:17:17 -0300

    troca de onchage para opnclick correçã de redirecionamento

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,12 +2,14 @@
 import "./Button.css"
 
 const Button = ({ id, text, action }) => {
-  // const handleAction = (e) => {
-  //   action(e)
-  // }
+  const handleAction = (e) => {
+    action(e)
+  }
   return (
-    <button id={id} onClick={action}>{text}</button>
-  )
+    <button id={id} onClick={handleAction}>
+      {text}
+    </button>
+  );
 }
 
 export default Button
